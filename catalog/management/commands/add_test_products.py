5a814b66ded540f509ab6b7be257159c332d7629
Said Magomedov <saidmagomedov037@gmail.com>
2025-06-13 20:04:13 +0500

    Проект подключен к СУБД PostgreSQL В приложение catalog созданы модели Products и Category, для них описаны базовые настройки Модели были перенесены в базу данных с помощью миграции Настроена админка Сформированы фикстуры для обеих моделей Создана кастомная команда для добавления тестовых продуктов Результаты работ с shell добавлены в папку skreenshots В контроллер отображения главной страницы добавлена возможность выводить в консоль 5 последних созданных продуктов

---
+++ catalog/management/commands/add_test_products.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from catalog.models import Products, Category
+
+class Command(BaseCommand):
+    help = 'Add test products to the database'
+
+    def handle(self, *args, **kwargs):
+        Products.objects.all().delete()
+        Category.objects.all().delete()
+
+        call_command('loaddata', 'catalog.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Products
 
 def home(request):
+    latest_products = Products.objects.order_by('-created_at')[:5]
+    for product in latest_products:
+        print(product)
     return render(request, 'home.html')
+
 
 def contacts(request):
     if request.method == 'GET':
